9be4cac69bb9185edfd0a73a43bd672276ec3bc5
Elijah Grey <elijah@grey.name>
2009-07-30 20:49:36 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,41 @@
+RetargetMouseScroll implements a simple API to retarget mouse scroll events.
+
+Usage
+-----
+
+    RetargetMouseScroll(
+        element         :Node,    // default: document
+        targetWindow    :Window,  // default: window
+        preventDefault  :Boolean, // default: true
+        scrollMultiplier:Number   // default: 1.0
+   )
+
+`RetargetMouseScroll` returns an object containing a `restore` method. Calling the method restores the default scrolling.
+
+Examples
+--------
+
+ * `RetargetMouseScroll(myElement, myFrame)` - Per-element mouse scroll retargetting to a frame
+ * `RetargetMouseScroll(document, window, true, 0.5)` - Slow down scrolling
+ * `RetargetMouseScroll(document, window, true, -1)` - Invert scrolling
+ * `RetargetMouseScroll(document, window, true, 2)` - Speed up scrolling
+
+More advanced example using a popup:
+
+    var win      = window.open("/", "example", "w="+screen.availWidth/3.5+",h="+screen.availHeight/3.5+",scrollbars=yes"),
+    retargetting = RetargetMouseScroll(document, win),
+    closedCheck  = setInterval(function() {
+    	if (win.closed) {
+    		retargetting.restore();
+    		clearInterval(closedCheck);
+    	}
+    }, 1000);
+
+In the previous example, all mouse scrolling on the main document is retargetted to the popup until it is closed.
+
+Supported Browsers \*
+--------------------
+
+* Firefox 3.5
+
+**\*** *I know the list is terribly small, I'll add more browsers soon*
---
+++ RetargetMouseScroll.js
@@ -0,0 +1,70 @@
+/*
+* RetargetWindowMouseScroll JavaScript Library v0.0.1
+* 2009-07-30
+* By Elijah Grey, http://eligrey.com
+*
+* http://github.com/eligrey/RetargetMouseScroll/tree/master
+*
+* License: GNU GPL v3 and the X11/MIT license
+*   See COPYING.md or http://eligrey.com/blog/about/license
+*/
+
+if (typeof this.RetargetMouseScroll !== "function") (function() {
+	var mouseScrollEvents = ["DOMMouseScroll", "mousescroll"];
+	
+	function handleScroll (evt, targetWindow, preventDefault, scrollMultiplier) {
+		if (preventDefault && evt.preventDefault)
+			evt.preventDefault();
+		
+		var scrollAmount = evt.detail
+		                   || (-evt.wheelDelta / 40); // convert wheelData to lines
+		scrollAmount *= 19; // convert lines to pixels
+		
+		if (typeof scrollMultiplier === "number" && !isNaN(scrollMultiplier))
+			scrollAmount *= scrollMultiplier;
+		
+		if (evt.wheelDeltaX || ("axis" in evt && "HORIZONTAL_AXIS" in evt && evt.axis == evt.HORIZONTAL_AXIS))
+			// horizontal scroll
+			targetWindow.scrollBy(scrollAmount, 0);
+		else // vertical scroll
+			targetWindow.scrollBy(0, scrollAmount);
+	};
+	
+	this.RetargetMouseScroll = function (elem, targetWindow, preventDefault, scrollMultiplier) {
+		if (!elem)
+			elem = document;
+		
+		if (!targetWindow)
+			targetWindow = window;
+		
+		if (typeof preventDefault !== "boolean")
+			preventDefault = true;
+		
+		var addListener, removeListener, restoreFn,
+		handler = function (evt) {
+			evt = evt || window.event;
+			handleScroll(evt, targetWindow, preventDefault, scrollMultiplier);
+		};
+		
+		if (addListener = elem.addEventListener) {
+			addListener.call(elem, mouseScrollEvents[0], handler, false);
+			addListener.call(elem, mouseScrollEvents[1], handler, false);
+		}
+		else if (addListener = elem.attachEvent)
+			addListener.call(elem, "on"+mouseScrollEvents[1], handler);
+		
+		if (removeListener = elem.removeEventListener)
+			restoreFn = function () {
+				removeListener.call(elem, mouseScrollEvents[0], handler, false);
+				removeListener.call(elem, mouseScrollEvents[1], handler, false);
+			};
+		else if (removeListener = elem.detachEvent)
+			restoreFn = function () {
+				removeListener.call(elem, "on"+mouseScrollEvents[1], handler);
+			};
+		
+		return {
+			restore: restoreFn
+		};
+	};
+}).call(this);
